70dde80c5eeb4859b9cb9620e5f8a146b41de074
Geovane <geeo.sp@gmail.com>
2015-01-17 00:14:09 -0300

    Vamo laa!

--- app/src/main/java/com/example/geeo/fastapp/fastapp/SimonActivity.java
+++ app/src/main/java/com/example/geeo/fastapp/fastapp/SimonActivity.java
@@ -1,26 +1,24 @@
 package com.example.geeo.fastapp.fastapp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SimonActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_simon);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_simon, menu);
         return true;
     }
 
@@ -37,15 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    public void oneClick(View v){
-        Intent intent = new Intent(this, OneActivity.class);
-        finish();
-        startActivity(intent);
-    }
-    public void twoClick(View v){
-        Intent intent = new Intent(this, TwoActivity.class);
-        finish();
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/geeo/fastapp/fastapp/TwoActivity.java
+++ app/src/main/java/com/example/geeo/fastapp/fastapp/TwoActivity.java
@@ -1,17 +1,23 @@
 package com.example.geeo.fastapp.fastapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class TwoActivity extends ActionBarActivity {
-
+    EditText et_player1_name;
+    EditText et_player2_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+        this.et_player1_name = (EditText)findViewById(R.id.et_player1_name);
+        this.et_player2_name = (EditText)findViewById(R.id.et_player2_name);
     }
 
 
@@ -35,5 +41,13 @@ public class TwoActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void openVelha(View v){
+        Intent intent = new Intent(this, VelhaActivity.class);
+        String player1Name = et_player1_name.getText().toString();
+        String player2Name = et_player2_name.getText().toString();
+        intent.putExtra("Player1",player1Name );
+        intent.putExtra("Player2",player2Name );
+        startActivity(intent);
     }
 }
